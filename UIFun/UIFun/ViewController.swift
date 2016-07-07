2dbb7067805c187bb60e4150d67cbef7567eec0c
eldooon <eldonchan91@gmail.com>
2016-07-07 16:29:36 -0400

    🖌  done - I'm terrible, didn't use mixColors this time...

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -11,17 +11,47 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
 
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstSegment: UISegmentedControl!
+    @IBOutlet weak var secondSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Set the initial paint color to "red"
+        paintBucket.backgroundColor = UIColor.redColor()
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
         // (e.g., "red", "purple", "blue")
+        
+        return "NOPE";
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+        switch (firstSegment.selectedSegmentIndex, secondSegment.selectedSegmentIndex){
+            
+        case (0,0):
+            paintBucket.backgroundColor = UIColor.redColor()
+        case (0,1):
+            paintBucket.backgroundColor = UIColor.orangeColor()
+        case (0,2):
+            paintBucket.backgroundColor = UIColor.purpleColor()
+        case (1,0):
+            paintBucket.backgroundColor = UIColor.orangeColor()
+        case (1,1):
+            paintBucket.backgroundColor = UIColor.yellowColor()
+        case (1,2):
+            paintBucket.backgroundColor = UIColor.greenColor()
+        case (2,0):
+            paintBucket.backgroundColor = UIColor.purpleColor()
+        case (2,1):
+            paintBucket.backgroundColor = UIColor.greenColor()
+        case (2,2):
+            paintBucket.backgroundColor = UIColor.blueColor()
+        default :
+            paintBucket.backgroundColor = UIColor.whiteColor()
+        }
     }
 }
